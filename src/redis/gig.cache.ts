ae9840538ce7d1181cfb90f9a5cd4e7d883bc936
oladapo <oladapoelijah49@gmail.com>
2025-05-17 22:09:15 +0100

    a logic to get data and save to redis

---
+++ src/redis/gig.cache.ts
@@ -0,0 +1,21 @@
+import { winstonLogger } from '@eoladapo/jobman-shared';
+import { config } from '@gig/config';
+import { Logger } from 'winston';
+import { client } from '@gig/redis/redis.connection';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'GigCache', 'debug');
+
+const getUserSelectedGigCategory = async (key: string): Promise<string> => {
+  try {
+    if (!client.isOpen) {
+      await client.connect();
+    }
+    const response: string = (await client.GET(key)) as string;
+    return response;
+  } catch (error) {
+    log.log('error', 'GigService getUserSelectedGigCategory() method:', error);
+    return '';
+  }
+};
+
+export { getUserSelectedGigCategory };
