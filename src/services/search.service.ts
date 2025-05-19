50972317d561048a2730539e54eff75b51be98c7
oladapo <oladapoelijah49@gmail.com>
2025-05-19 14:00:35 +0100

    impl topRatedgigByCategory

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -1,8 +1,8 @@
 import { elasticSearchClient } from '@gig/elasticsearch';
 import { SearchResponse } from '@elastic/elasticsearch/lib/api/types';
-import { IHitsTotal, ISearchResult } from '@eoladapo/jobman-shared';
+import { IHitsTotal, IPaginateProps, ISearchResult } from '@eoladapo/jobman-shared';
 
-export async function gigsSearchBySellerId(searchQuery: string, active: boolean): Promise<ISearchResult> {
+export const gigsSearchBySellerId = async (searchQuery: string, active: boolean): Promise<ISearchResult> => {
   const queryList: any[] = [
     {
       query_string: {
@@ -30,4 +30,161 @@ export async function gigsSearchBySellerId(searchQuery: string, active: boolean)
     total: total.value,
     hits: result.hits.hits
   };
-}
+};
+
+export const gigsSearch = async (
+  searchQuery: string,
+  paginate: IPaginateProps,
+  deliveryTime?: string,
+  min?: number,
+  max?: number
+): Promise<ISearchResult> => {
+  const { from, size, type } = paginate;
+  const queryList: any[] = [
+    {
+      query_string: {
+        fields: ['username', 'title', 'description', 'basicDescription', 'basicTitle', 'categories', 'subCategories', 'tags'],
+        query: `*${searchQuery}*`
+      }
+    },
+    {
+      term: {
+        active: true
+      }
+    }
+  ];
+
+  if (deliveryTime !== 'undefined') {
+    queryList.push({
+      query_string: {
+        fields: ['expectedDelivery'],
+        query: `*${deliveryTime}*`
+      }
+    });
+  }
+
+  if (!isNaN(parseInt(`${min}`)) && !isNaN(parseInt(`${max}`))) {
+    queryList.push({
+      range: {
+        price: {
+          gte: min,
+          lte: max
+        }
+      }
+    });
+  }
+  const result: SearchResponse = await elasticSearchClient.search({
+    index: 'gigs',
+    size,
+    query: {
+      bool: {
+        must: [...queryList]
+      }
+    },
+    sort: [
+      {
+        sortId: type === 'forward' ? 'asc' : 'desc'
+      }
+    ],
+    ...(from !== '0' && { search_after: [from] })
+  });
+  const total: IHitsTotal = result.hits.total as IHitsTotal;
+  return {
+    total: total.value,
+    hits: result.hits.hits
+  };
+};
+
+export const gigsSearchByCategory = async (searchQuery: string): Promise<ISearchResult> => {
+  const result: SearchResponse = await elasticSearchClient.search({
+    index: 'gigs',
+    size: 10,
+    query: {
+      bool: {
+        must: [
+          {
+            query_string: {
+              fields: ['categories'],
+              query: `*${searchQuery}*`
+            }
+          },
+          {
+            term: {
+              active: true
+            }
+          }
+        ]
+      }
+    }
+  });
+  const total: IHitsTotal = result.hits.total as IHitsTotal;
+  return {
+    total: total.value,
+    hits: result.hits.hits
+  };
+};
+
+export const getMoreGigsLikeThis = async (gigId: string): Promise<ISearchResult> => {
+  const result: SearchResponse = await elasticSearchClient.search({
+    index: 'gigs',
+    size: 5,
+    query: {
+      more_like_this: {
+        fields: ['title', 'description', 'basicDescription', 'basicTitle', 'categories', 'subCategories', 'tags'],
+        like: [
+          {
+            _index: 'gigs',
+            _id: gigId
+          }
+        ],
+        min_term_freq: 1,
+        max_query_terms: 12
+      }
+    }
+  });
+  const total: IHitsTotal = result.hits.total as IHitsTotal;
+  return {
+    total: total.value,
+    hits: result.hits.hits
+  };
+};
+
+export const getTopRatedGigsByCategory = async (searchQuery: string): Promise<ISearchResult> => {
+  const result: SearchResponse = await elasticSearchClient.search({
+    index: 'gigs',
+    size: 10,
+    query: {
+      bool: {
+        filter: {
+          script: {
+            script: {
+              source: 'doc["ratingSum"].value != 0 && (doc["ratingSum"].value / doc["ratingCount"].value) == params)',
+              lang: 'painless',
+              params: {
+                threshold: 5
+              }
+            }
+          }
+        },
+        must: [
+          {
+            query_string: {
+              fields: ['categories'],
+              query: `*${searchQuery}*`
+            }
+          },
+          {
+            term: {
+              active: true
+            }
+          }
+        ]
+      }
+    }
+  });
+  const total: IHitsTotal = result.hits.total as IHitsTotal;
+  return {
+    total: total.value,
+    hits: result.hits.hits
+  };
+};
